4211af194c4552ab51f2c9c96f9b82f981052884
Kavyansh Gupta <kavyanshgupta.work@gmail.com>
2025-02-15 11:03:59 +0530

    updated faculty attributes.

--- college_backend/models/Faculty.js
+++ college_backend/models/Faculty.js
@@ -1,1 +1,49 @@
+const mongoose = require("mongoose");
 
+const Faculty = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: false,
+    },
+    email: {
+        type: String,
+        required: false,
+    },
+    facultyId: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        private: true,
+    },
+    dept: {
+        type: String,
+        default: "",
+    },
+    specialization: {
+        type: String,
+        default: "",
+    },    
+    fatherName: {
+        type: String,
+        default: "",
+    },
+    motherName: {
+        type: String,
+        default: "",
+    },
+    address: {
+        type: String,
+        default: "",
+    }
+});
+
+const facultyModel = mongoose.model("Faculty" , Faculty);
+
+module.exports = facultyModel;
